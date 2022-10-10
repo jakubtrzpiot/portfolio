fedf7904413d14f8c971df261f0317ff7250704a
Jakub Trzpiot <j.trzpiot03@gmail.com>
2022-10-10 02:28:59 +0200

    misinput THERE WAS A MISINPUT

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,14 +1,12 @@
 import Navbar from './organisms/Navbar';
 import Footer from './organisms/Footer';
 
-const Layout = ({ children }) => {
-  return (
-    <>
-      <Navbar />
-      <main className="grow mt-20 pb-4">{children}</main>
-      <Footer />
-    </>
-  );
-};
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    <main className="grow mt-20 pb-4">{children}</main>
+    <Footer />
+  </>
+);
 
 export default Layout;
